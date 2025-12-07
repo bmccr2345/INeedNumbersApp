3724cb42582057453ebfb5fed92c74bcbc1b748c
Byron McCreary <bmccr23@gmail.com>
2025-12-07 12:04:59 -0500

    Sync iOS after bundle ID fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.inneednumbers.app',
+  appId: 'com.ineednumbers.app',
   appName: 'INeedNumbers',
   webDir: 'dist',
   version: '1.0',
